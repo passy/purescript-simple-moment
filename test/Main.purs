7b93a19462f2778850fc419b73bd85b82816b064
Pascal Hartig <phartig@rdrei.net>
2016-05-03 23:44:35 +0100

    WIP: Upgrade attempts

--- test/Main.purs
+++ test/Main.purs
@@ -1,8 +1,10 @@
 module Test.Main where
 
-import Prelude
-import Test.Unit
-import Test.Unit.Console (TestOutput())
+import Prelude (Unit, bind, ($), not)
+import Test.Unit (test, suite)
+import Test.Unit.Main (runTest)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Assert as Assert
 
 import Global (infinity)
 import Data.Date (Now(), now)
@@ -12,40 +14,38 @@ import Data.Maybe.Unsafe (fromJust)
 import Data.Time (Milliseconds(..))
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Unsafe (unsafeInterleaveEff)
-import qualified Data.Moment.Simple as M
-import qualified Data.Moment.Simple.Relative as M
-import qualified Data.Moment.Simple.Internal as M
 
-main :: forall eff. Eff ( testOutput :: TestOutput
+import Data.Moment.Simple as M
+import Data.Moment.Simple.Relative as M
+import Data.Moment.Simple.Internal as M
+
+main :: forall eff. Eff ( testOutput :: TESTOUTPUT
                         , now :: Now
                         | eff ) Unit
 main = do
   now' <- now
   runTest $ do
     test "valid" $ do
-      assert "now is always valid" $
+      Assert.assert "now is always valid" $
         M.isValid (M.fromDate now')
 
-      assert "0 is valid" $
+      Assert.assert "0 is valid" $
         isJust $ M.fromEpoch $ Milliseconds 0.0
 
-      assert "∞ is invalid" $
+      Assert.assert "∞ is invalid" $
         isNothing $ M.fromEpoch $ Milliseconds infinity
 
     test "relative" $ do
-      assertFn "fromNow contains ago" $ \done -> do
+      Assert.assert "fromNow contains ago" do
         timeStr <- unsafeInterleaveEff $ M.fromNow $ M.fromDate now'
-        done $ "ago" `contains` timeStr
+        "ago" `contains` timeStr
 
-      assertFn "fromNow' doesn't contain ago" $ \done -> do
+      Assert.assert "fromNow' doesn't contain ago" do
         timeStr <- unsafeInterleaveEff $ M.fromNow' $ M.fromDate now'
-        done $ not $ "ago" `contains` timeStr
+        not $ "ago" `contains` timeStr
 
     test "format" $ do
-      assert "ISO-8601" $ do
-        let d = fromJust $ M.fromEpoch $ Milliseconds 0.0
-        M.formatUTCISO8601 d == "1970-01-01T00:00:00+00:00"
+      let d = fromJust $ M.fromEpoch $ Milliseconds 0.0
 
-      assert "custom format" $ do
-        let d = fromJust $ M.fromEpoch $ Milliseconds 0.0
-        M.formatUTC "dddd, MMMM Do YYYY, h:mm:ss a" d == "Thursday, January 1st 1970, 12:00:00 am"
+      Assert.equal (M.formatUTCISO8601 d) "1970-01-01T00:00:00+00:00"
+      Assert.equal (M.formatUTC "dddd, MMMM Do YYYY, h:mm:ss a" d) "Thursday, January 1st 1970, 12:00:00 am"
