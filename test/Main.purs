5296df12431765bee7779486de8961ac246c03aa
Pascal Hartig <phartig@rdrei.net>
2016-05-04 22:46:55 +0100

    WIP: Test upgrades

--- test/Main.purs
+++ test/Main.purs
@@ -1,10 +1,11 @@
 module Test.Main where
 
-import Prelude (Unit, bind, ($), not)
+import Prelude
+import Control.Monad.Eff.Console (CONSOLE)
 import Test.Unit (test, suite)
-import Test.Unit.Main (runTest)
-import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Assert as Assert
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
 
 import Global (infinity)
 import Data.Date (Now(), now)
@@ -14,38 +15,37 @@ import Data.Maybe.Unsafe (fromJust)
 import Data.Time (Milliseconds(..))
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Unsafe (unsafeInterleaveEff)
+import Control.Monad.Eff.Class (liftEff)
 
 import Data.Moment.Simple as M
-import Data.Moment.Simple.Relative as M
-import Data.Moment.Simple.Internal as M
+import Data.Moment.Simple.Relative as MR
+import Data.Moment.Simple.Internal as MI
 
-main :: forall eff. Eff ( testOutput :: TESTOUTPUT
-                        , now :: Now
-                        | eff ) Unit
+main :: forall eff. Eff ( now :: Now
+                        , timer :: TIMER
+                        , avar :: AVAR
+                        , console :: CONSOLE
+                        , testOutput :: TESTOUTPUT | eff ) Unit
 main = do
   now' <- now
   runTest $ do
-    test "valid" $ do
-      Assert.assert "now is always valid" $
-        M.isValid (M.fromDate now')
+    suite "basics" do
+      test "validity" do
+        Assert.assert "now is always valid" $ MI.isValid (M.fromDate now')
+        Assert.assert "0 is valid" $ isJust $ M.fromEpoch $ Milliseconds 0.0
+        Assert.assert "∞ is invalid" $ isNothing $ M.fromEpoch $ Milliseconds infinity
 
-      Assert.assert "0 is valid" $
-        isJust $ M.fromEpoch $ Milliseconds 0.0
+    suite "relative" do
+      test "fromNow" do
+        timeStr <- liftEff $ MR.fromNow $ M.fromDate now'
+        Assert.assert "fromNow contains ago" $ "ago" `contains` timeStr
 
-      Assert.assert "∞ is invalid" $
-        isNothing $ M.fromEpoch $ Milliseconds infinity
+      test "fromNow'" do
+        timeStr <- liftEff $ MR.fromNow' $ M.fromDate now'
+        Assert.assertFalse "fromNow' doesn't contain ago" $ "ago" `contains` timeStr
 
-    test "relative" $ do
-      Assert.assert "fromNow contains ago" do
-        timeStr <- unsafeInterleaveEff $ M.fromNow $ M.fromDate now'
-        "ago" `contains` timeStr
-
-      Assert.assert "fromNow' doesn't contain ago" do
-        timeStr <- unsafeInterleaveEff $ M.fromNow' $ M.fromDate now'
-        not $ "ago" `contains` timeStr
-
-    test "format" $ do
+    test "format" do
       let d = fromJust $ M.fromEpoch $ Milliseconds 0.0
 
-      Assert.equal (M.formatUTCISO8601 d) "1970-01-01T00:00:00+00:00"
-      Assert.equal (M.formatUTC "dddd, MMMM Do YYYY, h:mm:ss a" d) "Thursday, January 1st 1970, 12:00:00 am"
+      Assert.equal "1970-01-01T00:00:00Z" (M.formatUTCISO8601 d)
+      Assert.equal "Thursday, January 1st 1970, 12:00:00 am" (M.formatUTC "dddd, MMMM Do YYYY, h:mm:ss a" d)
